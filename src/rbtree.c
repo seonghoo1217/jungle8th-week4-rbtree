e27454b5ee36417d2edcb4ab8906fe131168d65d
Lee.t.c <lsh6451217@gmail.com>
2024-04-09 01:40:11 +0900

    feat: insert fixed up 및 minmax 로직 변경, 중위순회 구현

--- src/rbtree.c
+++ src/rbtree.c
@@ -39,6 +39,99 @@ void delete_node(rbtree *t, node_t *node) {
     free(node);
 }
 
+void left_rotate(rbtree *t, node_t *x) {
+    node_t *y;
+    y = x->right;
+    x->right = y->left;
+    if (y->left != t->nil) {
+        y->left->parent = x;
+    }
+    y->parent = x->parent;
+    if (x->parent == t->nil) {
+        t->root = y;
+    } else if (x == x->parent->left) {
+        x->parent->left = y;
+    } else {
+        x->parent->right = y;
+    }
+    y->left = x;
+    x->parent = y;
+    return;
+}
+
+void right_rotate(rbtree *t, node_t *x) {
+    node_t *y;
+    y = x->left;
+    x->left = y->right;
+    if (y->right != t->nil) {
+        y->right->parent = x;
+    }
+    y->parent = x->parent;
+    if (x->parent == t->nil) {
+        t->root = y;
+    } else if (x == x->parent->right) {
+        x->parent->right = y;
+    } else {
+        x->parent->left = y;
+    }
+    y->right = x;
+    x->parent = y;
+    return;
+}
+
+void rb_insert_Fixup(rbtree *t, node_t *z) {
+    node_t *uncle;
+    // while ((z != t->root) && (z->color != RBTREE_BLACK) && (z->parent->color == RBTREE_RED))
+    while (z->parent->color == RBTREE_RED) {
+        if (z->parent == z->parent->parent->left) {
+            uncle = z->parent->parent->right;
+            //경우1
+            if (uncle->color == RBTREE_RED) {
+                z->parent->color = RBTREE_BLACK;
+                uncle->color = RBTREE_BLACK;
+                z->parent->parent->color = RBTREE_RED;
+                z = z->parent->parent;
+            }
+                //경우2
+            else {
+                if (z == z->parent->right) {
+                    z = z->parent;
+                    left_rotate(t, z);
+                }
+                //경우3
+                z->parent->color = RBTREE_BLACK;
+                z->parent->parent->color = RBTREE_RED;
+                right_rotate(t, z->parent->parent);
+            }
+        }
+            //반대로
+        else {
+            uncle = z->parent->parent->left;
+            //경우1
+            if (uncle != t->nil && uncle->color == RBTREE_RED) {
+                z->parent->color = RBTREE_BLACK;
+                uncle->color = RBTREE_BLACK;
+                z->parent->parent->color = RBTREE_RED;
+                z = z->parent->parent;
+            }
+                //경우2
+            else {
+                if (z == z->parent->left) {
+                    z = z->parent;
+                    right_rotate(t, z);
+                }
+                //경우3
+                if (z != t->root && z->parent != t->root) {
+                    z->parent->color = RBTREE_BLACK;
+                    z->parent->parent->color = RBTREE_RED;
+                    left_rotate(t, z->parent->parent);
+                }
+            }
+        }
+    }
+    t->root->color = RBTREE_BLACK;
+}
+
 node_t *rbtree_insert(rbtree *t, const key_t key) {
     // TODO: implement insert
     node_t *newNode = (node_t *) calloc(1, sizeof(node_t));
@@ -71,7 +164,11 @@ node_t *rbtree_insert(rbtree *t, const key_t key) {
 
     if (current == t->nil) {
         t->root = newNode;
+        newNode->color = RBTREE_BLACK;
     }
+
+    rb_insert_Fixup(t, newNode);
+
     return t->root;
 }
 
@@ -102,9 +199,10 @@ node_t *rbtree_find(const rbtree *t, const key_t key) {
 node_t *rbtree_min(const rbtree *t) {
     // TODO: implement find
     node_t *current = t->root;
+
     if (current == t->nil)
-        return current;
-    while (current != t->nil) {
+        return t->nil;
+    while (current->left != t->nil) {
         current = current->left;
     }
     return current;
@@ -113,6 +211,7 @@ node_t *rbtree_min(const rbtree *t) {
 node_t *tree_minimum(const rbtree *t, node_t *sub_root) {
     // TODO: implement find
     node_t *r = sub_root;
+
     if (r == t->nil)
         return r;
     while (r->left != t->nil) {
@@ -124,7 +223,10 @@ node_t *tree_minimum(const rbtree *t, node_t *sub_root) {
 node_t *rbtree_max(const rbtree *t) {
     // TODO: implement find
     node_t *current = t->root;
-    while (current != t->nil) {
+
+    if (current == t->nil)
+        return t->nil;
+    while (current->right != t->nil) {
         current = current->right;
     }
     return current;
@@ -176,14 +278,44 @@ int rbtree_erase(rbtree *t, node_t *z) {
         y->color = z->color;
     }
 
-    if (y_orginal_color == RBTREE_BLACK) {
+    if (y_original_color == RBTREE_BLACK) {
         //TODO : 더블리 블랙
     }
     free(z);
     return 0;
 }
 
+int inorder_rbtree(node_t *root, key_t *res, const rbtree *t, int index) {
+    if (root == t->nil) {
+        return index;
+    }
+    index = inorder_rbtree(root->left, res, t, index);
+    res[index] = root->key;
+    printf("inorder index : %d \n", res[index]);
+    index += 1;
+    index = inorder_rbtree(root->right, res, t, index);
+    return index;
+}
+
 int rbtree_to_array(const rbtree *t, key_t *arr, const size_t n) {
     // TODO: implement to_array
+    if (t->root == t->nil) {
+        return -1;
+    }
+    inorder_rbtree(t->root, arr, t, 0);
     return 0;
 }
+
+/*int main() {
+    rbtree *newTree = new_rbtree();
+//    rbtree_insert(newTree, 1);
+//    rbtree_insert(newTree, 2);
+//    rbtree_insert(newTree, 3);
+//    rbtree_insert(newTree, 4);
+//    rbtree_insert(newTree, 5);
+    key_t arr1[] = {10, 5, 8, 34, 67, 23, 156, 24, 2, 12, 24, 36, 990, 25};
+    const size_t n1 = sizeof(arr1) / sizeof(arr1[0]);
+    key_t *res1 = calloc(n1, sizeof(key_t));
+    rbtree_to_array(newTree, res1, n1);
+    delete_rbtree(newTree);
+}*/
